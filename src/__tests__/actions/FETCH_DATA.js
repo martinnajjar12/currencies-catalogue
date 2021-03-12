36ce46e903f9e076d3c88bc6d7a8b0cd3ddb2eea
Martin Najjar <martin_najjar@yahoo.com>
2021-03-12 22:31:58 +0300

    Install moxios and redux-mock-store to test the FETCH DATA action

--- src/__tests__/actions/FETCH_DATA.js
+++ src/__tests__/actions/FETCH_DATA.js
@@ -1,9 +1,35 @@
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import moxios from 'moxios';
 import FETCH_DATA from '../../actions/FETCH_DATA';
 
-jest.mock('../../actions/FETCH_DATA');
+const baseUrl = `https://api.nomics.com/v1/currencies/ticker?key=${process.env.REACT_APP_API_KEY}&per-page=25&interval=1d&convert=USD&sort=rank`;
+
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
 
 describe('FETCH_DATA', () => {
-  it('should return an object', () => {
-    expect(FETCH_DATA()).toReturn(typeof Function);
+  beforeEach(() => {
+    moxios.install();
+  });
+
+  afterEach(() => {
+    moxios.uninstall();
+  });
+
+  it('should return an object when fetching successfully', () => {
+    moxios.stubOnce('GET', baseUrl, {
+      status: 200,
+      response: [{ name: 'test 1', rank: '1' }, { name: 'test 2', rank: '2' }],
+    });
+
+    const store = mockStore({ name: 'test 1', rank: '1' }, { name: 'test 2', rank: '2' });
+
+    return store.dispatch(FETCH_DATA()).then(() => {
+      expect(store.getActions()).toEqual([{
+        type: 'FETCH DATA',
+        payload: [{ name: 'test 1', rank: '1' }, { name: 'test 2', rank: '2' }],
+      }]);
+    });
   });
 });
--- src/actions/FETCH_DATA.js
+++ src/actions/FETCH_DATA.js
@@ -2,16 +2,12 @@ import axios from 'axios';
 
 const baseUrl = `https://api.nomics.com/v1/currencies/ticker?key=${process.env.REACT_APP_API_KEY}&per-page=25&interval=1d&convert=USD&sort=rank`;
 
-const FETCH_DATA = () => {
-  const request = axios.get(baseUrl);
-  return dispatch => {
-    request.then(({ data }) => {
-      dispatch({
-        type: 'FETCH DATA',
-        payload: data,
-      });
-    });
-  };
-};
+const FETCH_DATA_SUCCESSFUL = data => ({
+  type: 'FETCH DATA',
+  payload: data,
+});
+
+const FETCH_DATA = () => dispatch => axios.get(baseUrl)
+  .then(({ data }) => dispatch(FETCH_DATA_SUCCESSFUL(data)));
 
 export default FETCH_DATA;
